1d38e7931bd6d414b0fd6a59b6a73e44c27fa4a2
Usernameorsmth <simidarawilliams@gmial.com>
2025-05-21 19:01:07 +0100

    CLOWN

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-def weather_condition():
-    print("weather is pleasent in", spring)
-    print("weather is same in", autumn)
+def add(P, Q):
+    return P + Q
 
-spring = "autumn"
-autumn = "winter"
-weather_condition()
+def subtract(P, Q):
+    return P - Q
+
+def multiply(P, Q):
+    return P * Q
+
+def divide(P, Q):
+    return P / Q
+
+print("Please select operation.")
+print("a. Add")
+print("b. Subtract")
+print("c. Multiply")
+print("d. Divide")
+
+choice = input("Please enter choice (a/b/c/d): ")
+
+num_1 = int(input("Please enter the first number: "))
+num_2 = int(input("Please enter the second number: "))
+
+if choice == 'a':
+    print(num_1, "+", num_2, "=", add(num_1, num_2))
+elif choice == 'b':
+    print(num_1, "-", num_2, "=", subtract(num_1, num_2))
+elif choice == 'c':
+    print(num_1, "*", num_2, "=", multiply(num_1, num_2))
+elif choice == 'd':
+    print(num_1, "/", num_2, "=", divide(num_1, num_2))
+else:
+    print("This is an invalid input")
